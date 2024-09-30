efc6ef45b55ebd8c067925f69057e76ca80f6eab
Gigi Gillen <gillen.g@northeastern.edu>
2024-09-30 11:30:56 +0100

    lab submission

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystems;
+//using UnityEngine.InputSystems;
 
 public class PlayerController : MonoBehaviour
 {
 
+
     public Vector2 moveValue; //public variable to store input movements
     public float speed; //public variable for speed
 
-    void OnMove(InputValue value) {
-        moveValue = value.Get<Vector2>(); // Retrieve the input value as a Vector2 and assign it to moveValu
+    void Start()
+    {
+        moveValue = new Vector2(0, 0); // Retrieve the input value as a Vector2 and assign it to moveValu
+
     }
 
     // called on a fixed time interval, used for physics calculations
     void FixedUpdate() {
+       
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y); // Create a Vector3 for movement based on the input values
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime); // Apply force to the Rigidbody to move the player
